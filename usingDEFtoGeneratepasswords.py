82d540652af711a94b7c0dfd7a8296075f69f2de
hey <no>
2024-02-19 14:36:38 +0100

    initial commit

---
+++ usingDEFtoGeneratepasswords.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+
+
+
+
+def gen_password():
+    num = int(input("Enter number of passwords: "))
+    len_of_passwords = int(input("Enter length of passswords: "))
+    #password = [] 
+    for i in range(num):
+        passwords = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(len_of_passwords))
+        print(passwords)
+
+gen_password()
+
+
