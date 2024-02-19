907ab38a62ca2efe08f007d386448d4b2d90939a
chinedutimothy <67475731+chinedutimothy@users.noreply.github.com>
2024-02-19 14:40:33 +0100

    Update usingDEFtoGeneratepasswords.py

--- usingDEFtoGeneratepasswords.py
+++ usingDEFtoGeneratepasswords.py
@@ -1,16 +1,16 @@
 import random
 import string
 
-
+print("Generating Passwords")
 
 
 
 def gen_password():
-    num = int(input("Enter number of passwords: "))
-    len_of_passwords = int(input("Enter length of passswords: "))
-    #password = [] 
-    for i in range(num):
+    num_of_passwords = int(input("Enter number of passwords to be generated: "))
+    len_of_passwords = int(input("Enter length of passswords to be generated: ")) 
+    for i in range(num_of_passwords):
         passwords = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(len_of_passwords))
+        print("Here is/are your password(s)")
         print(passwords)
 
 gen_password()
