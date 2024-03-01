007c05a3323a101de03aa1c89ade8240a985b73f
chinedutimothy <67475731+chinedutimothy@users.noreply.github.com>
2024-03-01 15:00:19 +0100

    Update GuessingNumberswithWhileloop.py

--- GuessingNumberswithWhileloop.py
+++ GuessingNumberswithWhileloop.py
@@ -1,78 +1,23 @@
-
 import random 
 
 print("Welcome to Guessing number game")
 
 def guess(x):
     random_number = random.randint(1, x)
-    print(random_number)
-    guess = 0
+    # print(random_number)  # Commenting out to keep the random number hidden
     limit = int(input("Input the number of guesses you're allowed to: "))
-    count = int(0)
-    for i in range(limit):
+    count = 0
+    while count < limit:
         guess = int(input(f"Guess a number between 1 and {x}: "))
-        count += int(1)
-        if guess != random_number:
-            if guess > random_number and count < limit:
-                print("Your guess is high \n keep guessing")
-            elif guess < random_number and count < limit:
-                print("Your guess is low \n Keep guessing")
-        elif guess == random_number and count < limit:
+        count += 1
+        if guess == random_number:
             print("You guessed correctly")
-                    
-    print("You're out of guesses")
-        
-
-    
-       
-        
+            return
+        elif count < limit:  # Only give hints if guesses are left
+            if guess > random_number:
+                print("Your guess is high!!!\nKeep guessing")
+            else:
+                print("Your guess is low!!!\nKeep guessing")
+    print("Out of guesses")
 
 guess(15)
-
-
-
-        
-
-    
-
-       
-            
-            
-
-           
-
-   
-            
-
-        
-              
-
-
-    
-
-
-        
-
-
-                
-
-
-
-                    
-                
-           
-
-            
-
-        
-            
-        
-
- 
-    
-
-
-
-    
-    
-
